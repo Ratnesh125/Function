51c84c6026fee359c843aaa6dbdad59e75256823
Ratnesh125 <87529730+Ratnesh125@users.noreply.github.com>
2022-10-25 23:16:36 +0530

    function declaration

--- function declaration.cpp
+++ function declaration.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
-int getMax(int ,int); //here we have to only declare the type of data it is name is not important,its upto us.
+// #declaration
+int getMax(int ,int); 
+//here we have to only declare the type of data it is name is not important,its upto us.
 //and also return type of the function 
 //we can also just declare the function without implementing its working (for later use)
+//but if you keep the declaration and remove defination then compiler throw linker error that the defination for called function is not found
 int main() {
     int x=10,y=20;
     cout<<getMax(x,y);
     return 0;
 }
+// #defination
 int getMax(int x ,int y){
     if(x>y)
     return x;
